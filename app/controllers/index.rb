911e5e5d34d28a9bb31f50198ecc51126ddf2eab
Jeremy Sell <thestereoscopics@gmail.com>
2013-08-22 11:30:11 -0700

    update for heroku

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,6 +9,5 @@ post '/color' do
   color= "#" + "%06x" % (rand * 0xffffff)
   
   { background: color, box: cell }.to_json
-  #Create and return a JSON object with the random cell and color given below.
-  # erb :color_change
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,32 +1,33 @@
 $(document).ready(function(){
   $('#get_color').on('click', function(e) {
-    console.log("click");
+    // console.log("click");
     e.preventDefault();
 
 
 
     var request = $.ajax({
       type: 'post',
-      url: '/color',
+      url: '/color'
     } );
 
-    request.done(function(server_data) {
-      console.log(server_data.background);
-      console.log(server_data.box);
+    request.done(function(data) {
+      //console.log(data.background);
+      //console.log(data.box);
       //console.log(json);
-      $('#color_me').find('li:nth-child('+server_data.box+')').css("background-color", ""+server_data.background+"");
+      $('#color_me').find('li:nth-child('+data.box+')').css("background-color", data.background);
     });
 
     request.fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
       console.log("not working");
     });
 
     request.always(function(arg1, arg2, arg3) {
-      //console.log("always");
+      // console.log("always");
+      // console.log(arg2);
     });
 
   });
 });
 
 
-// $('ul li:nth-child(random_number)').css("background-color", "random_color")
